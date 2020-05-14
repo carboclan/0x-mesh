dcafddda6c9165abc7c884a1343df95423d8aa5f
Andrei Anisimov <1471231+andrei-anisimov@users.noreply.github.com>
2020-05-14 16:31:02 -0700

    dynamic MarketContractProxyAddress

--- zeroex/ordervalidator/order_validator.go
+++ zeroex/ordervalidator/order_validator.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"math/big"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 	"sync"
@@ -256,6 +257,15 @@ func New(contractCaller bind.ContractCaller, chainID int, maxRequestContentLengt
 	if err != nil {
 		return nil, err
 	}
+	// Initialize Honeylemon MarketContractProxy
+	honeylemonMarketContractProxyAddress := os.Getenv("HONEYLEMON_MARKET_CONTARCT_PROXY_ADDRESS")
+	if len(honeylemonMarketContractProxyAddress) == 0 {
+		return nil, errors.New("Envar HONEYLEMON_MARKET_CONTARCT_PROXY_ADDRESS is empty")
+	}
+	marketContractProxy, err := wrappers.NewMarketContractProxyCaller(common.HexToAddress(honeylemonMarketContractProxyAddress), contractCaller)
+	if err != nil {
+		return nil, err
+	}
 	assetDataDecoder := zeroex.NewAssetDataDecoder()
 
 	return &OrderValidator{
@@ -405,7 +415,22 @@ func (o *OrderValidator) BatchValidate(ctx context.Context, rawSignedOrders []*z
 				}
 
 				fillableMakerAmounts, err := o.marketContractProxy.GetFillableAmounts(opts, addresses)
-				// TODO: handle errors
+				if err != nil {
+					log.Error("Could not fetch fillable amount from MarketContractProxy")
+					// Reject all orders
+					for j, orderInfo := range results.OrdersInfo {
+						orderHash := common.Hash(orderInfo.OrderHash)
+						signedOrder := signedOrders[j]
+						validationResults.Rejected = append(validationResults.Rejected, &RejectedOrderInfo{
+							OrderHash:   orderHash,
+							SignedOrder: signedOrder,
+							Kind:        MeshError,
+							Status:      ROEthRPCRequestFailed,
+						})
+					}
+					return
+				}
+
 				for i, address := range addresses {
 					fillableMakerAmountsMap[address] = fillableMakerAmounts[i]
 				}
